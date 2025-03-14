0aa386b12cb6ea6c0a0f35f29b814aec61e57f5f
Jockson Mateus <jocksonmatheus@gmail.com>
2025-03-14 08:39:19 -0300

    change project

--- src/sections/projects.jsx
+++ src/sections/projects.jsx
@@ -7,7 +7,7 @@ const Projects = () => {
             <div className="flex flex-col space-y-5 md:space-y-20 mt-10 pb-10">
                 <ul className="flex flex-col flex-wrap space-y-5 md:space-y-0 md:flex-row justify-center md:justify-between">
                     <Card link={'https://eraumavez.vercel.app/'} title={'Era uma vez'} description={'Langing page real comercial'} url={'https://i.postimg.cc/fRx8m71B/project2.webp'}/>
-                    <Card link={'https://jockaplay-todolist.netlify.app/'} title={'ToDo App'} description={'Aplicativo de anotação de tarefas'} url={'https://i.postimg.cc/qB5DTv8X/project1.webp'}/>
+                    <Card link={'https://www.brilliantsoftware.com.br/'} title={'Brilliant Software'} description={'Site para minha empresa'} url={'https://i.postimg.cc/tCcwJg0S/Captura-de-tela-2025-03-14-083752.png'}/>
                     <Card link={'https://responsive-login-screen.vercel.app/'} title={'Tela de login'} description={'Tela de login responsiva'} url={'https://i.postimg.cc/9Fw8FHb3/project3.webp'}/>
                 </ul>
                 <div className="md:flex justify-center"><a target="_blank" href="https://github.com/jockaplay?tab=repositories" className="flex justify-center md:w-2/12 bg-purple-950 rounded-2xl text-white p-5 md:p-2 md:hover:scale-110 transition md:hover:bg-purple-900">Ver Mais</a></div>
